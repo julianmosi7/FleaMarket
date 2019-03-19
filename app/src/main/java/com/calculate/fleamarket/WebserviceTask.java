204127e0eb1cf48735bfde04919352ec7dbd79e6
julianmosi7 <moshammer.ju@gmail.com>
2019-03-19 09:23:12 +0100

    first commit

---
+++ app/src/main/java/com/calculate/fleamarket/WebserviceTask.java
@@ -0,0 +1,15 @@
+package com.calculate.fleamarket;
+
+import android.os.AsyncTask;
+
+public class WebserviceTask extends AsyncTask<String, Integer, String> {
+
+    @Override
+    protected String doInBackground(String... strings) {
+        String username = strings[0];
+        String sJson = "";
+        try{
+            
+        }
+    }
+}
