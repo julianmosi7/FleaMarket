43a0330760cccf17be476a2333686c5f8353cc74
julianmosi7 <moshammer.ju@gmail.com>
2019-03-25 22:26:21 +0100

    save

--- app/src/main/java/com/calculate/fleamarket/MainActivity.java
+++ app/src/main/java/com/calculate/fleamarket/MainActivity.java
@@ -2,13 +2,16 @@ package com.calculate.fleamarket;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -31,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
     private ListView listView;
     private ArrayAdapter<Article> adapter;
     private List<Article> articles = new ArrayList();
+    private SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +43,14 @@ public class MainActivity extends AppCompatActivity {
 
         listView = findViewById(R.id.listView);
 
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
 
 
         bindAdapterToListView(listView);
@@ -66,13 +78,23 @@ public class MainActivity extends AppCompatActivity {
      }
 
     public class WebserviceTask extends AsyncTask<String, Integer, String> {
+        String art = "";
+        String op;
+
+        public WebserviceTask(String[] article){
+            String operation = article[0];
+            art = operation;
+            if(operation.equals("add")){
+                art = art + "&name=" + article[1] + "&price=" + article[2] + "&username=" + article[3] + "&password=" + article[4] + "&email=" + article[5] + "&phone=" + article[6];
+            }
+            System.out.println("------------");
+            System.out.println(art);
+            op = operation;
+        }
 
         @Override
         protected String doInBackground(String... strings) {
-            String operation = "";
-            String username = strings[0];
-            int password = 17333;
-            String url = "http://eaustria.no-ip.biz/flohmarkt/flohmarkt.php?operation=get";
+            String url = "http://eaustria.no-ip.biz/flohmarkt/flohmarkt.php?operation="+art;
             String sJson = "";
             try {
                 HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
@@ -105,28 +127,39 @@ public class MainActivity extends AppCompatActivity {
         }
 
         private void outputRepoNames(String s) {
-            try {
-                JSONObject jsonObject = new JSONObject(s);
-                JSONArray array = jsonObject.getJSONArray("data");
-                for (int i = 0; i < array.length(); i++) {
-                    jsonObject = array.getJSONObject(i);
-                    int id = Integer.parseInt(jsonObject.getString("id"));
-                    String name = jsonObject.getString("name");
-                    double price = Double.parseDouble(jsonObject.getString("price"));
-                    String username = jsonObject.getString("username");
-                    String email = jsonObject.getString("email");
-                    String phone = jsonObject.getString("phone");
-                    Article article = new Article(id, name, price, username, email, phone);
-                    articles.add(article);
-                    for (Article art :
-                            articles) {
-                        System.out.println(art.toString());
-                    }
-                }
-            } catch (JSONException ex) {
-                ex.printStackTrace();
-            }
-            adapter.notifyDataSetChanged();
+           if(op.equals("add")){
+               try {
+                   JSONObject jsonObject = new JSONObject(s);
+                   jsonObject.getString("code");
+                   jsonObject.getString("data");
+                   showWebserviceTask();
+               } catch (JSONException ex) {
+                   ex.printStackTrace();
+               }
+               adapter.notifyDataSetChanged();
+           }else{
+               try {
+                   JSONObject jsonObject = new JSONObject(s);
+                   JSONArray array = jsonObject.getJSONArray("data");
+                   for (int i = 0; i < array.length(); i++) {
+                       jsonObject = array.getJSONObject(i);
+                       int id = Integer.parseInt(jsonObject.getString("id"));
+                       String name = jsonObject.getString("name");
+                       double price = Double.parseDouble(jsonObject.getString("price"));
+                       String username = jsonObject.getString("username");
+                       String email = jsonObject.getString("email");
+                       String phone = jsonObject.getString("phone");
+                       Article article = new Article(id, name, price, username, email, phone);
+                       articles.add(article);
+                   }
+               } catch (JSONException ex) {
+                   ex.printStackTrace();
+               }
+               adapter.notifyDataSetChanged();
+
+           }
+
+
         }
     }
 
@@ -138,7 +171,9 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void showWebserviceTask(){
-        WebserviceTask task = new WebserviceTask();        
+        String[] art = new String[1];
+        art[0] = "get";
+        WebserviceTask task = new WebserviceTask(art);
         task.execute(username);
     }
 
@@ -151,12 +186,23 @@ public class MainActivity extends AppCompatActivity {
         final EditText txtusername = vDialog.findViewById(R.id.username);
         final EditText txtemail = vDialog.findViewById(R.id.email);
         final EditText txttelephone = vDialog.findViewById(R.id.telephone);
+        final String[] art = new String[10];
+
+
         alert.setView(vDialog);
 
         alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                //add
+                art[0] = "add";
+                art[1] = txtdescription.getText().toString();
+                art[2] = txtprice.getText().toString();
+                art[3] = txtusername.getText().toString();
+                art[4] = prefs.getString("password", "17333");
+                art[5] = txtemail.getText().toString();
+                art[6] = txttelephone.getText().toString();
+                WebserviceTask task = new WebserviceTask(art);
+                task.execute();
             }
         });
 
@@ -166,6 +212,29 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+
+
+        alert.show();
+    }
+
+    public void dialog_show(){
+        final View vDialog = getLayoutInflater().inflate(R.layout.view, null);
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+
+
+
+        alert.setView(vDialog);
+
+       alert.setNeutralButton("OK", new DialogInterface.OnClickListener() {
+           @Override
+           public void onClick(DialogInterface dialog, int which) {
+
+           }
+       });
+
+
 
         alert.show();
     }
