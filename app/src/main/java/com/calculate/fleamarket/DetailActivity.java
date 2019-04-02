d8f0dffdf77c7ba0e815b824ea4f7512e0fdb9a5
julianmosi7 <moshammer.ju@gmail.com>
2019-04-02 12:19:33 +0200

    activity finished-buttons required now

--- app/src/main/java/com/calculate/fleamarket/DetailActivity.java
+++ app/src/main/java/com/calculate/fleamarket/DetailActivity.java
@@ -3,15 +3,31 @@ package com.calculate.fleamarket;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
+    Article article;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
+        article = (Article) bundle.getSerializable("article");
+        show();
+    }
 
-        Article article = (Article) bundle.getSerializable("article");
+    public void show(){
+        final TextView description = findViewById(R.id.bezeichnung);
+        final TextView price = findViewById(R.id.preis);
+        final TextView username = findViewById(R.id.benutzername);
+        final TextView email = findViewById(R.id.email);
+        final TextView telephone = findViewById(R.id.telefon);
+
+        description.setText(article.getDescription());
+        price.setText(String.valueOf(article.getPrice()));
+        username.setText(article.getUsername());
+        email.setText(article.getEmail());
+        telephone.setText(article.getTelephone());
     }
 }
