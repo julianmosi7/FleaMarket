ab7805337a94ebeb9ab183e8a7a79f24ee6688dc
julianmosi7 <moshammer.ju@gmail.com>
2019-05-13 16:52:45 +0200

    flea

--- app/src/main/java/com/calculate/fleamarket/DetailActivity.java
+++ app/src/main/java/com/calculate/fleamarket/DetailActivity.java
@@ -1,5 +1,6 @@
 package com.calculate.fleamarket;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
@@ -7,9 +8,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class DetailActivity extends AppCompatActivity {
     Article article;
+    String mail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +20,7 @@ public class DetailActivity extends AppCompatActivity {
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
         article = (Article) bundle.getSerializable("article");
+
         show();
 
         Button b = findViewById(R.id.call);
@@ -24,6 +28,14 @@ public class DetailActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 openPhoneCall();
+            }
+        });
+
+        Button c = findViewById(R.id.email_to);
+        c.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openEmail();
             }
         });
 
@@ -41,6 +53,7 @@ public class DetailActivity extends AppCompatActivity {
         username.setText(article.getUsername());
         email.setText(article.getEmail());
         telephone.setText(article.getTelephone());
+        mail = email.getText().toString();
 
     }
 
@@ -49,5 +62,28 @@ public class DetailActivity extends AppCompatActivity {
         Uri uri = Uri.parse(phone);
         Intent intent = new Intent(Intent.ACTION_DIAL, uri);
         startActivity(intent);
+    }
+
+    public void openEmail() {
+        String email = mail;
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setData(Uri.parse("mailto:"));
+        intent.setType("text/plain");
+       intent.putExtra(Intent.EXTRA_EMAIL, email);
+       intent.putExtra(Intent.EXTRA_SUBJECT, "Subject");
+       intent.putExtra(Intent.EXTRA_TEXT, "Message:");
+
+       try{
+           startActivity(intent.createChooser(intent, "Send mail..."));
+           finish();
+       }catch(ActivityNotFoundException ex){
+           Toast.makeText(this, "No email client installed.", Toast.LENGTH_SHORT).show();
+       }
+
+
+
+
+
+
     }
 }
